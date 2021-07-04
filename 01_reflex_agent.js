27939736b4fce987e660f25f26a6d11a5eb82155
Javier Barreda <javier.barreda94@gmail.com>
2021-07-03 22:18:01 -0600

    reflex agent fix

--- 01_reflex_agent.js
+++ 01_reflex_agent.js
@@ -1,28 +1,32 @@
 // MIT License
 // Copyright (c) 2020 Luis Espino
 
-function reflex_agent(location, state){
-   	if (state=="DIRTY") return "CLEAN";
-   	else if (location=="A") return "RIGHT";
-   	else if (location=="B") return "LEFT";
+function reflex_agent(location, state) {
+	if (state == "DIRTY") return "CLEAN";
+	else if (location == "A") return "RIGHT";
+	else if (location == "B") return "LEFT";
 }
 
-function test(states){
-      	var location = states[0];		
-      	var state = states[0] == "A" ? states[1] : states[2];
-      	var action_result = reflex_agent(location, state);
-      	document.getElementById("log").innerHTML+="<br>Location: ".concat(location).concat(" | Action: ").concat(action_result);
-      	if (action_result == "CLEAN"){
-        	if (location == "A") states[1] = "CLEAN";
-         	else if (location == "B") states[2] = "CLEAN";
-      	}
-      	else if (action_result == "RIGHT") states[0] = "B";
-      	else if (action_result == "LEFT") states[0] = "A";
-		if(states[1]=="CLEAN" && states[2]=="CLEAN"){
-			document.getElementById("log").innerHTML+="<br>Location: ".concat(location).concat(" | Action: ENSUCIANDO AMBOS ").concat(action_result);
-		}		
-	setTimeout(function(){ test(states); }, 2000);
+function test(states) {
+	
+	var location = states[0];
+	var state = states[0] == "A" ? states[1] : states[2];
+	var action_result = reflex_agent(location, state);
+	document.getElementById("log").innerHTML += "<br>Location: ".concat(location).concat(" | Action: ").concat(action_result);
+	if (action_result == "CLEAN") {
+		if (location == "A") states[1] = "CLEAN";
+		else if (location == "B") states[2] = "CLEAN";
+	}
+	else if (action_result == "RIGHT") states[0] = "B";
+	else if (action_result == "LEFT") states[0] = "A";
+
+	if (states[1] == "CLEAN" && states[2] == "CLEAN") {
+		states[1] = "DIRTY";
+		states[2] = "DIRTY";
+		document.getElementById("log").innerHTML += "<br>Location: ".concat(location).concat(" | Action: ENSUCIANDO AMBOS ")
+	}
+	setTimeout(function () { test(states); }, 2000);
 }
 
-var states = ["A","DIRTY","DIRTY"];
+var states = ["A", "DIRTY", "DIRTY"];
 test(states);
